5def9b77a85d1441e5d945b020e13716418e219f
Vinay Pillai <vinaypillai01@gmail.com>
2020-10-20 12:03:49 -0700

    fix(dnsresolver_new.py): readded mapname call

--- test/dns_resolver_test.py
+++ test/dns_resolver_test.py
@@ -7,7 +7,7 @@ import json
 if __name__ == "__main__":
     # resolver = DNSResolver([{'addr':"192.172.226.186",'port':1080}])
     resolver = DNSResolver()
-    domain_dict = resolver.get_domain_dict("10794.earth.orderbox-dns.com")
+    domain_dict = resolver.get_domain_dict("google.com")
     # domain_dict = resolver.get_domain_dict("kitchengardenstore.com")
     print(json.dumps(domain_dict))
 
