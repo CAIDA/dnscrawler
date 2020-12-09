94d8fb64de3589eff57e86a2b7b4532c15f44c97
Vinay Pillai <vinaypillai01@gmail.com>
2020-12-09 14:24:06 -0800

    feat(dns_resolver_test.py): add duration calculation to dns_resolver_test

--- test/dns_resolver_test.py
+++ test/dns_resolver_test.py
@@ -3,19 +3,23 @@ sys.path.append("../")
 from dnscrawler import DNSResolver, load_schema
 from dnscrawler.logger import log
 import json
-
+import time
 if __name__ == "__main__":
+    start_time = int(time.time())
     # resolver = DNSResolver([{'addr':"192.172.226.186",'port':1080}])
     resolver = DNSResolver(ipv4_only=True)
     # resolver = DNSResolver()
-    domain_dict = resolver.get_domain_dict("google.com")
+    # domain_dict = resolver.get_domain_dict("google.com")
     # Empty non-terminal
     # domain_dict = resolver.get_domain_dict("caag.state.ca.us")
     # Hazardous domain
     # domain_dict = resolver.get_domain_dict("PREGNANCYCALCULATE.COM.")
     # Cross zone loops
-    # domain_dict = resolver.get_domain_dict("amazon.com")
+    domain_dict = resolver.get_domain_dict("amazon.com")
     # Early ns records
     # domain_dict = resolver.get_domain_dict("aridns.net.au")
+    finish_time = int(time.time())
+    duration = finish_time - start_time
     print(json.dumps(domain_dict))
+    print(f"Duration:{duration}")
 
